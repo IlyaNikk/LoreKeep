30cc1aa19daad806c4f6e1ae5afe6722f1bf447a
IlyaNikk <vic096@yandex.ru>
2017-03-25 17:11:31 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.export = {
+	entry: [
+		'./public/main.js'
+	],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: path.join('assets', 'js', 'bundle.js'),
+		publicPath: '/'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['latest']
+				}
+			},
+			{
+				test: /\.css$/,
+				loader: "css-loader"
+			}
+		]
+	},
+	resolve: {
+		alias: {}
+	},
+	plugins: [
+		new CleanWebpackPlugin('dist'),
+		new webpack.LoaderOptionsPlugin({
+			debug: true
+		}),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new HtmlPlugin({
+			filename: 'index.html',
+			template: path.resolve(__dirname, 'public/index.html')
+		})
+	]
+};
